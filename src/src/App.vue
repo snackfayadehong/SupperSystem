4013cc756d2d3b3947cc769de7038bc41d506b1b
fayadehong <snackchina@163.com>
2023-05-31 11:26:23 +0800

    顶部导航栏

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
-
 <template>
     <div></div>
+    <navigation-bar-top></navigation-bar-top>
 </template>
+
+<script setup>
+import NavigationBarTop from "./components/NavigationBarTop/NavigationBarTop.vue";
+</script>
 
 <style scoped></style>
