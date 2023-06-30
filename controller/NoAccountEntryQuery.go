89c0751bfd99abdca317682d0b7065ba7be67a6a
fayadehong <snackchina@163.com>
2023-06-30 17:49:58 +0800

    调拨未上账查询

--- controller/NoAccountEntryQuery.go
+++ controller/NoAccountEntryQuery.go
@@ -8,7 +8,7 @@ import (
 
 func NoAccountEntryQuery(endTime *string) (bills *[]model.NoAccountEntry) {
 	clientDb.DB.Raw(clientDb.NoAccountEntrySql, endTime).Find(&bills)
-	for i, _ := range *bills {
+	for i := range *bills {
 		tempTime, _ := time.Parse("2006-01-02T15:04:05Z", (*bills)[i].BLDate)
 		(*bills)[i].BLDate = tempTime.Format("2006-01-02 15:04:05")
 	}
