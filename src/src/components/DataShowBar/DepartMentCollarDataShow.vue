e88b8346e22fdd8f8ac5c616acd1878110076938
fayadehong <snackchina@163.com>
2023-07-03 18:01:17 +0800

    未上账查询界面

---
+++ src/src/components/DataShowBar/DepartMentCollarDataShow.vue
@@ -0,0 +1,19 @@
+<template>
+    <el-table :data="tableData" style="width: 100%" :border="true">
+        <el-table-column fixed prop="date" label="单号" />
+        <el-table-column prop="name" label="制单人" />
+        <el-table-column prop="state" label="领用科室" />
+        <el-table-column prop="city" label="领用人" />
+        <el-table-column prop="address" label="出库科室" />
+        <el-table-column prop="zip" label="库房审核时间" />
+    </el-table>
+</template>
+
+<script setup name="departmentCollar">
+import { ref } from "vue";
+import bus from "../../eventBus";
+const tableData = ref();
+bus.on("getData", res => {
+    tableData.value = res;
+});
+</script>
