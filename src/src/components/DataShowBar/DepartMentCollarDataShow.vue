1bc93154ee1a03b5e22960e5161310d647130744
fayadehong <snackchina@163.com>
2023-07-05 16:49:22 +0800

    增加数据序号

--- src/src/components/DataShowBar/DepartMentCollarDataShow.vue
+++ src/src/components/DataShowBar/DepartMentCollarDataShow.vue
@@ -1,6 +1,7 @@
 <template>
     <el-table :data="tableData" style="width: 100%" :border="true" class="elTable">
-        <el-table-column fixed prop="DepartmentCollarCode" label="单号" align="center" />
+        <el-table-column fixed prop="Index" label="序号" width="50" align="center" />
+        <el-table-column prop="DepartmentCollarCode" label="单号" align="center" />
         <el-table-column prop="BLMakerName" label="制单人" align="center" />
         <el-table-column prop="LeadingDepartmentName" label="领用科室" align="center" />
         <el-table-column prop="LeaderName" label="领用人" align="center" />
@@ -12,10 +13,10 @@
 <script setup name="departmentCollar">
 import { ref } from "vue";
 import bus from "../../eventBus";
+// 表格数据
 const tableData = ref([]);
 bus.on("getDepartmenCollarData", res => {
     tableData.value = res;
-    console.log(res);
 });
 </script>
 
