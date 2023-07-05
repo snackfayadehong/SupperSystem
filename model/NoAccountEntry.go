59b104514b06ed69dec1db6a7d4f5b7d5409e256
fayadehong <snackchina@163.com>
2023-07-05 16:53:02 +0800

    注释

--- model/NoAccountEntry.go
+++ model/NoAccountEntry.go
@@ -1,6 +1,8 @@
 package model
 
+// NoAccountEntry 调拨未上账单据模型
 type NoAccountEntry struct {
+	Index                  int    // 序号
 	DepartmentCollarCode   string `gorm:"column:DepartmentCollarCode"` // 单号
 	BLDate                 string `gorm:"BLDate"`                      // 库房审核时间
 	LeadingDepartmentName  string `gorm:"LeadingDepartmentName"`       // 领用科室
