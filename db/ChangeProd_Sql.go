d348d43efe0a032d29fed384a170bd89217a05df
fayadehong <snackchina@163.com>
2024-01-26 17:41:28 +0800

    优化字典查询sql

--- db/ChangeProd_Sql.go
+++ db/ChangeProd_Sql.go
@@ -6,13 +6,19 @@ package clientDb
 
 // QueryProd 查询产品信息
 const QueryProd = `SELECT
-	prod.CODE -- 院内代码
+	prod.Code -- 院内代码
 	,prod.HisProductCode3 AS HospitalName -- 院内名称
 	,prod.HospitalSpec -- 院内规格
 	,prod.HisProductCode7 AS YGCGID -- 网采平台产品ID
 	,prod.HisProductCode7Source -- 集采平台产品临时ID 审核过后会改变HisProductCode7值
 	,prod.HisProductCode7Status -- 集采平台产品ID状态
 	,prod.CusCategoryCode -- 104分类编码
+	,
+	CASE
+	WHEN cus.SubjectGrade = 0 THEN '' 
+	WHEN cus.SubjectGrade = 1 THEN '00' 
+	ELSE SUBSTRING ( prod.CusCategoryCode, 1, ( CONVERT ( INT, cus.SubjectGrade ) - 1 ) * 2 ) + '0000'
+	END ParentCusCategoryCode
 	,td.TenderCode AS TradeCode -- 交易编码
 	,yb.MedicareCode -- 医保编码
 	,jc.SysCode -- 集采系统编码
@@ -20,15 +26,16 @@ const QueryProd = `SELECT
 	,prod.IsVoid -- 0：启用 1：停用
 	,ep.PurState -- 0：供货 1：停止供货
 FROM
-	TB_ProductInfo prod
-	LEFT JOIN TB_TenderCode td ON td.ProductInfoID = prod.ProductInfoID 
+	TB_ProductInfo  prod WITH (NOLOCK)
+	LEFT JOIN TB_TenderCode td  WITH (nolock) ON td.ProductInfoID = prod.ProductInfoID 
 	AND td.IsVoid = 0 
 	AND td.MedicareType = 1
-	LEFT JOIN TB_ProductChargeRule yb ON yb.ProductInfoID = prod.ProductInfoID 
+	LEFT JOIN TB_ProductChargeRule yb WITH (nolock) ON yb.ProductInfoID = prod.ProductInfoID 
 	AND td.IsVoid = 0 
 	AND yb.MedicareType = 1
-	LEFT JOIN TB_EnterpriseProduct ep ON ep.ProductID = prod.ProductInfoID 
+	LEFT JOIN TB_EnterpriseProduct ep  WITH (nolock) ON ep.ProductID = prod.ProductInfoID 
 	AND prod.DefaultSupplierID = ep.EnterpriseID
-	LEFT JOIN TB_ProductInfoJCSysCode jc ON jc.Prod_Id = prod.ProductInfoID 
+	LEFT JOIN TB_ProductInfoJCSysCode jc WITH (nolock) ON jc.Prod_Id = prod.ProductInfoID 
+	LEFT JOIN TB_ProductCustomCategory cus WITH (nolock) on prod.CusCategoryCode = cus.CusCategoryCode
 WHERE
 	prod.CODE IN (?)`
