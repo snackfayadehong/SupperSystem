7c2c0be21150e1a2cb1a66b2216649d27f368fe1
fayadehong <snackchina@163.com>
2026-01-26 14:19:54 +0800

    del no import

--- pkg/db/RefundRetry_Sql.go
+++
@@ -1,6 +0,0 @@
-package clientDb
-
-const QueryRefundBillno = `SELECT RetWarhouCode as yddh,'02' as rkfs FROM TB_Refund WHERE ISNULL(SendStatus,'') = '' 
-                                      and Status = 51  and  CreateTime >= ?  and createTime < ? `
-
-const UpdateRefund_Sql = ` UPDATE TB_Refund SET SendStatus = 1  where RetWarhouCode = ?`
